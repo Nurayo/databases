194a708a2740dc8943cbaae806bfb11f2126ba2d
Nurayo <144511522+Nurayo@users.noreply.github.com>
2024-09-18 14:10:28 +0500

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,1 +1,30 @@
-CREATE DATABSE
+create database lab1;
+
+CREATE TABLE users(
+    id SERIAL ,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
+);
+ALTER TABLE users
+ADD isadmin INT ;
+
+ALTER TABLE users
+ALTER COLUMN isadmin DROP DEFAULT;
+
+ALTER TABLE users
+ALTER COLUMN isadmin TYPE BOOLEAN USING( isadmin::BOOLEAN);
+
+ALTER TABLE users
+ALTER COLUMN isadmin SET DEFAULT false;
+
+ALTER TABLE users
+ADD PRIMARY KEY (id);
+
+CREATE TABLE tasks(
+    id SERIAL,
+    name VARCHAR(50),
+    user_id INT
+);
+
+DROP TABLE tasks;
+DROP DATABASE lab1;
